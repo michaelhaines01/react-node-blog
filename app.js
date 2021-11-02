893e215fef6b0a0a016f921a8090edf61974ac02
Michael Haines <michaelhaines01@gmail.com>
2021-11-03 09:38:42 +1100

    deploy to heroku

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+///app.use(express.static(path.resolve(__dirname, "../client/build")));
 //Passport middleware
 app.use(passport.initialize());
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+//app.get("*", (req, res) => {
+//res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+//});
 
 const indexRouter = require("./routes/index");
 const adminRouter = require("./routes/admin");
